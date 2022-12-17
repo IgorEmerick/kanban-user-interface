d9bbbe8fbc9fb118e419c19612875748f5ef8ea1
Igor Emerick <igorbarbosaemerick@gmail.com>
2022-12-17 15:58:39 -0300

    update login page files path

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Login } from '../pages/Login';
+import { Login } from '../pages/User/Login';
 
 export function AppRoutes(): JSX.Element {
   return (
